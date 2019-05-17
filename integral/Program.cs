e9a151216b5de795c8d46f32d9391b5971359650
Velly <breshchinskaya@mail.ru>
2019-05-17 21:35:10 +0300

    Добавьте файлы проекта.

---
+++ integral/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace integral
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
